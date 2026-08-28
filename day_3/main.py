0bb5e2a17c7223efdb5bf4191eedd58e9c72b396
DUNDI UMA VENKATA REDDY <23kq5a0514cse@gmail.com>
2026-08-29 00:57:17 +0530

    included middlware function from middleware.py

--- day_3/main.py
+++ day_3/main.py
@@ -1,14 +1,12 @@
-from fastapi import FastAPI,Request
-import time ,uuid
+from fastapi import FastAPI
 from products import router as product_router
 from orders import router as order_router
 from middleware import middleware
-
 app = FastAPI(title="Routing and path parameter and query parameter", description="it is day 2 learning modules form my 75 days fastapi learniing challenge. tooday's topics are routing and path parameters and query parameters.")
 
-@app.middleware("http")(middleware)
+app.add_middleware(middleware)
 
+app.include_router(product_router, prefix="/products", tags=["products"])
 
-@app.include_router(product_router, prefix="/products", tags=["products"])
+app.include_router(order_router, prefix="/orders", tags=["orders"])
 
-@app.include_router(order_router, prefix="/orders", tags=["orders"])
