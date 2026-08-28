762bd652bdc725846fe23f15e18a12897f8d14c2
DUNDI UMA VENKATA REDDY <23kq5a0514cse@gmail.com>
2026-08-29 00:00:55 +0530

    uploading modifies day 3 main.py

--- day_3/main.py
+++ day_3/main.py
@@ -5,18 +5,20 @@ from orders import router as order_router
 
 app = FastAPI(title="Routing and path parameter and query parameter", description="it is day 2 learning modules form my 75 days fastapi learniing challenge. tooday's topics are routing and path parameters and query parameters.")
 
-app.middleware("http")
+@app.middleware("http")
 async def middleware(request: Request, call_next):
-    start_time = time.pref_counter()
+    start_time = time.perf_counter()
 
-    print("Request")
+    print("========Request received========")
     print("Method:", request.method)
     print("path:", request.url.path)
+    print("headers:" , request.headers.get("user-agent"))
 
     response = await call_next(request)
 
-    print("Response")
+    print("========Response generated========")
     print("Status Code:", response.status_code)
+    print("Headers:", response.headers)
     end_time = time.perf_counter()
     process_time = end_time - start_time
     print("Process Time:", process_time)
