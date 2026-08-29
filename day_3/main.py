ed57445e5ac6303b4cf4ffa03c0c2a6ca7b1c56d
DUNDI UMA VENKATA REDDY <23kq5a0514cse@gmail.com>
2026-08-29 10:55:55 +0530

    added another directory middlware.py file into main.py

--- day_3/main.py
+++ day_3/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Request
 from products import router as product_router
 from orders import router as order_router
-from middleware import middleware
+from Middlware.middleware import middleware
 app = FastAPI(title="Routing and path parameter and query parameter", description="it is day 2 learning modules form my 75 days fastapi learniing challenge. tooday's topics are routing and path parameters and query parameters.")
 
 @app.middleware("http")
