a5a8249eb1e42a45adba4bd2f6e83ceb863a5442
DUNDI UMA VENKATA REDDY <23kq5a0514cse@gmail.com>
2026-09-18 16:35:55 +0530

    smll changes

--- day_6/config/settings.py
+++ day_6/config/settings.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict # type: ignore
 from pydantic import SecretStr
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
